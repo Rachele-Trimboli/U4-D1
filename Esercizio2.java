963793520d8c1f315fbc8bbb191ca2d938b05541
Rachele-Trimboli <146757214+Rachele-Trimboli@users.noreply.github.com>
2024-03-04 14:53:23 +0100

    Esercizio2

---
+++ Esercizio2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Esercizio2 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Dimmi il tuo nome");
+        String name = scanner.nextLine();
+        System.out.println("Dimmi il tuo cognome");
+        String cognome = scanner.nextLine();
+        System.out.println("Dimmi il tuo colore preferito");
+        String colore = scanner.nextLine();
+        System.out.println("il mio nome è: " + name + " il mio cognome è: " + cognome + " il mio colore preferito è: " + colore);
+
+
+
+
+
+    }
+
+
+}
